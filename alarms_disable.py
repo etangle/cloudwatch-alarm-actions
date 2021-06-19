26424be9731cac3207e94308f1c7de1d485acff7
etangle <shahid@etangle.com>
2021-06-19 13:14:11 +1000

    Create alarms_disable.py

---
+++ alarms_disable.py
@@ -0,0 +1,23 @@
+import boto3
+import os
+
+# create cloudwatch client
+cloudwatch = boto3.client('cloudwatch')
+# create sns client
+sns_client = boto3.client('sns')
+
+def lambda_handler(event, context):
+        # Take only specific alarms to disable, provide alarms w/out spaces i.e alarm1,alarm2,alarmn
+        # Initialize alarms list parameter
+        ALARMS = []
+        ALARMS = os.environ['ALARMS']
+        ALARMS = ALARMS.split(",")
+        for alarmname in ALARMS:
+            cloudwatch.disable_alarm_actions(AlarmNames=[alarmname])
+            print("Alarm: " + alarmname + " has been disabled!")
+        #Send SNS msg
+        response = sns_client.publish(
+                TopicArn = os.environ['SNS_TOPIC_ARN'],
+                Message = os.environ['ALARMS'],
+                Subject = "Alarm Disabled"
+                )
